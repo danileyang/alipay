cdcbe70a3202f0f8553710c013577580e93b1673
daniel <daniel.yonder@gmail.com>
2017-03-13 15:16:29 +0800

    打包alipay依赖,用户无需再引入alipaySDK

--- src/main/java/com/daniel/base/BaseResult.java
+++ src/main/java/com/daniel/base/BaseResult.java
@@ -10,8 +10,8 @@ import java.util.Map;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
-import static org.apache.commons.lang.StringUtils.isNotBlank;
-import static org.apache.commons.lang.StringUtils.trim;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static org.apache.commons.lang3.StringUtils.trim;
 
 /**
  * Created by daniel on 17/3/11.
